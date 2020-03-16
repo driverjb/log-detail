74a9b1913bc8c0eafd5748f2b7d7e5168b0ef6a9
Joel Driver <driverjb09@gmail.com>
2020-03-15 23:33:38 -0400

    collection of interfaces for configuration and levels

---
+++ src/interfaces.ts
@@ -0,0 +1,8 @@
+export type Level = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+export interface Config {
+  enabled?: boolean;
+  timestampFormat?: string;
+  level?: Level;
+  color?: boolean;
+  appName?: string;
+}
