677bda1a63f3b1fb56c34bb6135619b7ec5b610e
Joel Driver <driverjb09@gmail.com>
2020-03-25 08:35:59 -0400

    removed the ability to format the time - you get what i want

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,8 +2,6 @@ export type Level = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
 export interface Config {
   /** Enable/Disable logging globally */
   enabled?: boolean;
-  /** Refer to moment module for formatting string options */
-  timestampFormat?: string;
   /** Set the minimum level of logging for output */
   level?: Level;
   //color?: boolean;
